5a62b9c0e644851c100ecfb9f47b94e3904415ea
Francesco-Rapetti <rapettif80@yahoo.it>
2024-04-22 17:42:31 +0200

    Mod Author class

--- csharp-biblioteca/Author.cs
+++ csharp-biblioteca/Author.cs
@@ -20,8 +20,13 @@ namespace csharp_biblioteca
             this.Documents = new List<Document>();
         }
 
-        public void AddDocument(Document book) => Documents.Add(book);
+        public void AddDocument(Document input) 
+        {
+            if (this.Documents.Find(doc => doc.Id == input.Id) == null) Documents.Add(input);
+            else Console.WriteLine("Document already exists");
+        }
         public string GetListDocuments() => string.Join(", ", Documents);
-    }
+        public override string ToString() => $"{Name} {Surname}";
     }
 }
+
