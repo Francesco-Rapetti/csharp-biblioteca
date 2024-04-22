2b091394298c0a8cba80533bbf1cffb282c8ed84
Francesco-Rapetti <rapettif80@yahoo.it>
2024-04-22 14:57:02 +0200

    Add User class

---
+++ csharp-biblioteca/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+
+        public User(string name, string surname, string email, string password, string phone)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Email = email;
+            this.Password = password;
+            this.Phone = phone;
+        }
+    }
+}
