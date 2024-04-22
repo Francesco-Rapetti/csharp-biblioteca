32f8c77df11e00b91f463d10b9ecddb8c77eb97c
Francesco-Rapetti <rapettif80@yahoo.it>
2024-04-22 15:13:41 +0200

    Add Author class

---
+++ csharp-biblioteca/Author.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Author
+    {
+        public string Name { get; }
+        public string Surname { get; }
+        public List<Book> WrittenBooks { get; }
+               
+
+        public Author(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.WrittenBooks = new List<Book>();
+        }
+
+        public void AddWrittenBook(Book book) => WrittenBooks.Add(book);
+        public string GetListWrittenBooks() => string.Join(", ", WrittenBooks);
+    }
+    }
+}
