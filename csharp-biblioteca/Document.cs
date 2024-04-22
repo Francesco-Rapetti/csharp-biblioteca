e30a33908b8128838f764b1fc56314b8df80dab8
Francesco-Rapetti <rapettif80@yahoo.it>
2024-04-22 15:21:41 +0200

    Add Document class

---
+++ csharp-biblioteca/Document.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Document
+    {
+        public string Id { get; }
+        public string Title { get; }
+        public int Year { get; }
+        public Category Category { get; set; }
+        public char Shelf { get; }
+        public Author Author { get; }
+
+        public Document(string title, int year, Category category, Author author)
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Title = title;
+            this.Year = year;
+            this.Category = category;
+            this.Author = author;
+            this.Shelf = Author.Surname[0];
+        }
+    }
+
+
+
+    public enum Category
+    {
+        HISTORY,
+        MATHEMATICS,
+        ECONOMY,
+        OTHER
+    }
+}
