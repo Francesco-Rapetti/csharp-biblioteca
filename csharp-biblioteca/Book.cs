31847f95910e5509f939ece1d06011f584efe829
Francesco-Rapetti <rapettif80@yahoo.it>
2024-04-22 17:44:12 +0200

    Add Book class

---
+++ csharp-biblioteca/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Book : Document
+    {
+        public int Pages { get; }
+
+        public Book(string title, int year, Category category, Author author, int pages) : base(title, year, category, author) 
+        {
+            this.Pages = pages;
+            author.AddDocument(this);
+        }
+    }
+}
