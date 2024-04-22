4e64d3eeaedb7424203c2b9b40c9bf1fc5a22a4b
Francesco-Rapetti <rapettif80@yahoo.it>
2024-04-22 17:44:19 +0200

    Add DVD class

---
+++ csharp-biblioteca/DVD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class DVD : Document
+    {
+        public double Duration { get; set; }
+        public DVD(string title, int year, Category category, Author author, double duration) : base(title, year, category, author)
+        {
+            this.Duration = duration;
+            author.AddDocument(this);
+        }
+    }
+}
